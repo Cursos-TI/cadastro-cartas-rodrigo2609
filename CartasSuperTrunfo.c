81ee169319c6b36333a0a05a118b87069d6303f1
rodrigo2609 <rodrigoerickleda1@gmail.com>
2025-10-17 12:46:12 -0300

    rodrigocartassupertrunfo
    This update implements a card creation system for cities, allowing users to input various city attributes such as state, code, name, population, area, GDP, and tourist points. It also displays the entered data for two cards.

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,15 +1,94 @@
 #include <stdio.h>
 
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das cartas
-// Objetivo: No nível novato você deve criar as cartas representando as cidades utilizando scanf para entrada de dados e printf para exibir as informações.
+int main(){
 
-int main() {
-  // Área para definição das variáveis para armazenar as propriedades das cidades
+char Estado1[3], Estado2[3];
+char Codigo1[20], Codigo2[20];
+char Nome_da_cidade1[20], Nome_da_cidade2[20];
+int Populacao1, Populacao2;
+float Area1, Area2;
+float PIB1, PIB2;
+int Pontos_turisticos1, Pontos_turisticos2;
 
-  // Área para entrada de dados
+printf("PREENCHA DADOS PARA AS CARTAS\n");
+printf("\n");
+printf("DADOS DA CARTA 1:\n");
 
-  // Área para exibição dos dados da cidade
+printf("Digite uma letra de estado entre A e H: ");
+scanf("%s", Estado1);
 
-return 0;
-} 
+printf("Digite um codigo ex:A01: ");
+scanf("%s", Codigo1);
+
+printf("Digite nome da cidade: ");
+scanf("%s", Nome_da_cidade1);
+
+printf("Digite a populacao da cidade em milhares: ");
+scanf("%d", &Populacao1);
+
+printf("Digite a area da cidade em km²: ");
+scanf("%f", &Area1);
+
+printf("Digite o PIB da cidade em bilhoes: ");
+scanf("%f", &PIB1);
+
+printf("Digite o numero de pontos turisticos da cidade: ");
+scanf("%d", &Pontos_turisticos1);
+
+printf("\n");
+
+printf("DADOS DA CARTA 2:\n");
+printf("Digite uma letra de estado entre A e H: ");
+scanf("%s", Estado2);
+
+printf("Digite um codigo ex:A01: ");
+scanf("%s", Codigo2);
+
+printf("Digite nome da cidade: ");
+scanf("%s", Nome_da_cidade2);
+
+printf("Digite a populacao da cidade em milhares: ");
+scanf("%d", &Populacao2);
+
+printf("Digite a area da cidade em km²: ");
+scanf("%f", &Area2);
+
+printf("Digite o PIB da cidade em bilhoes: ");
+scanf("%f", &PIB2);
+
+printf("Digite o numero de pontos turisticos da cidade: ");
+scanf("%d", &Pontos_turisticos2);
+
+printf("\n");
+//IMPRIMIR CARTAS
+
+printf("CARTA 1:\n");
+printf("Estado: %s\n", Estado1);
+printf("Codigo: %s\n", Codigo1);
+printf("Cidade: %s\n", Nome_da_cidade1);
+printf("Populacao: %d mil habitantes\n", Populacao1);
+printf("Area: %.0f km²\n", Area1);
+printf("PIB: %.1f bilhoes\n", PIB1);
+printf("Numero de pontos turisticos: %d\n", Pontos_turisticos1);
+
+printf("\n");
+
+printf("CARTA 2:\n");
+printf("Estado: %s\n", Estado2);
+printf("Codigo: %s\n", Codigo2);
+printf("Cidade: %s\n", Nome_da_cidade2);
+printf("Populacao: %d mil habitantes\n", Populacao2);
+printf("Area: %.0f km²\n", Area2);
+printf("PIB: %.1f bilhoes\n", PIB2);
+printf("Numero de pontos turisticos: %d\n", Pontos_turisticos2);
+
+printf("\n");
+printf("\n");
+printf("\n");
+printf("\n");
+
+printf("digite f,ENTER para finalizar: ");
+scanf("%s");
+
+
+}
